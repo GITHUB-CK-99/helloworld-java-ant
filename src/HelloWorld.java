f64b2f18363a8d1ba9d20daaa37a65df234aff80
GITHUB-CK-99 <160500779+GITHUB-CK-99@users.noreply.github.com>
2024-03-21 20:05:04 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World devops");
+        System.out.println("Hello, World-devops");
         }
 }
